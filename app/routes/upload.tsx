979a7a058956b83bf0bba65a4f8f430ef6726ed1
ahmedman47 <mansoutiahmed747@gmail.com>
2025-09-07 14:04:11 +0100

    resume feedback page

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -69,6 +69,7 @@ const Upload = () => {
         await kv.set(`resume:${uuid}` , JSON.stringify(data));
         setStatusText('Analysis Done, redirecting ...');
         console.log(data);
+        navigate(`/resume/${uuid}`);
 
     }
 
